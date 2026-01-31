b68aff277cc4bcca67283ed8e5bf03e4d040a99f
Letian Zheng <zlt_0503@sjtu.edu.cn>
2026-02-01 00:05:54 +0800

    chore: answer to Lab1

--- Lab1/kernel/arch/aarch64/boot/raspi3/init/mmu.c
+++ Lab1/kernel/arch/aarch64/boot/raspi3/init/mmu.c
@@ -87,16 +87,34 @@ void init_kernel_pt(void)
         /* TTBR1_EL1 0-1G */
         /* LAB 1 TODO 5 BEGIN */
         /* Step 1: set L0 and L1 page table entry */
-        /* BLANK BEGIN */
-        /* BLANK END */
+	vaddr = KERNEL_VADDR + PHYSMEM_START;
+        boot_ttbr1_l0[GET_L0_INDEX(vaddr)] = ((u64)boot_ttbr1_l1) | IS_TABLE
+                                             | IS_VALID | NG;
+        boot_ttbr1_l1[GET_L1_INDEX(vaddr)] = ((u64)boot_ttbr1_l2) | IS_TABLE
+                                             | IS_VALID | NG;
 
         /* Step 2: map PHYSMEM_START ~ PERIPHERAL_BASE with 2MB granularity */
-        /* BLANK BEGIN */
-        /* BLANK END */
+        for (; vaddr < KERNEL_VADDR + PERIPHERAL_BASE; vaddr += SIZE_2M) {
+                boot_ttbr1_l2[GET_L2_INDEX(vaddr)] =
+                        (vaddr - KERNEL_VADDR) /* high mem, va = KERNEL_VADDR + pa */
+                        | UXN /* Unprivileged execute never */
+                        | ACCESSED /* Set access flag */
+                        | NG /* Mark as not global */
+                        | INNER_SHARABLE /* Shareability */
+                        | NORMAL_MEMORY /* Normal memory */
+                        | IS_VALID;
+        }
 
         /* Step 2: map PERIPHERAL_BASE ~ PHYSMEM_END with 2MB granularity */
-        /* BLANK BEGIN */
-        /* BLANK END */
+        for (vaddr = KERNEL_VADDR + PERIPHERAL_BASE; vaddr < KERNEL_VADDR + PHYSMEM_END; vaddr += SIZE_2M) {
+                boot_ttbr1_l2[GET_L2_INDEX(vaddr)] =
+                        (vaddr - KERNEL_VADDR) /* high mem, va = KERNEL_VADDR + pa */
+                        | UXN /* Unprivileged execute never */
+                        | ACCESSED /* Set access flag */
+                        | NG /* Mark as not global */
+                        | DEVICE_MEMORY /* Device memory */
+                        | IS_VALID;
+        }
         /* LAB 1 TODO 5 END */
 
         /*
